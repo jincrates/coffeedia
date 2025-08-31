69b7f6f8b7be8238d48e043df21e5864a7c0a4f4
jincrates <94jingyu@gmail.com>
2025-08-31 15:47:21 +0900

    refactor: 패키지 중간 depth 추가

--- backend/modules/bootstrap/gateway/src/main/java/io/coffeedia/bootstrap/gateway/GatewayApplication.java
+++ backend/modules/bootstrap/gateway/src/main/java/io/coffeedia/bootstrap/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package io.coffeedia.bootstrap;
+package io.coffeedia.bootstrap.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
